3da36bb7b93d0857ce1ecd37940872d66b923489
David Duan <davidduan0916@gmail.com>
2017-05-03 17:42:41 +0800

    Update App.java

--- app/src/main/java/com/avos/sms/App.java
+++ app/src/main/java/com/avos/sms/App.java
@@ -9,7 +9,7 @@ import com.avos.avoscloud.AVOSCloud;
 public class App extends Application {
   public void onCreate() {
     // 请用你的AppId，AppKey。并在管理台启用手机号码短信验证
-    AVOSCloud.initialize(this, "1ha2c8pykvoylq5xvngpfjhn4f7dcrczw0r3ui0f8ycbrc25",
-        "ip1ld5m0uc2f9phnknzw3jak014cx796tid24gq89vaw7w15");
+    AVOSCloud.initialize(this, "xxx",
+        "xxx");
   }
 }
